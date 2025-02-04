6e5b102c2f902f419ee56d255f1f54baca867532
Daffa Hakim <daffahakim62@gmail.com>
2025-02-04 09:51:12 +0700

    Create formModel.js

---
+++ formModel.js
@@ -0,0 +1,43 @@
+export class FormModelDataKTP {
+  constructor() {
+    this.nik = "";
+    this.namaLengkap = "";
+    this.tanggalLahir = "";
+    this.tempatLahir = "";
+    this.jenisKelamin = "";
+    this.agama = "";
+    this.alamat = "";
+    this.rt = "";
+    this.rw = "";
+    this.provinsi = "";
+    this.kota = "";
+    this.kecamatan = "";
+    this.kelurahan = "";
+    this.kodePos = "";
+    this.statusPerkawinan = "";
+    this.masaAktifKtp = "";
+    this.namaIbuKandung = "";
+  }
+}
+
+export class FormModelDataPribadi {
+  constructor() {
+    this.namaPanggilan = "";
+    this.tujuan = "";
+    this.kantorCabang = "";
+    this.alamatKantorCabang = "";
+    this.pendidikanTerakhir = "";
+    this.hobi = "";
+    this.nomorTelepon = "";
+    this.nomorFax = "";
+    this.alamatSesuaiEktp = "";
+    this.alamat = "";
+    this.rt = "";
+    this.rw = "";
+    this.provinsi = "";
+    this.kota = "";
+    this.kecamatan = "";
+    this.kelurahan = "";
+    this.kodePos = "";
+  }
+}
